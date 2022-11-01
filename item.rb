9af6e794aabf456ee7e4ca2055176153dd264a98
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-01 12:29:30 +0300

    Add class item attributes

--- item.rb
+++ item.rb
@@ -1,5 +1,24 @@
 class Item
-    def initialize (genre, author, )
-        
-    end
+  attr_reader :publish_date, :genre, :author, :source, :label
+
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
 end
