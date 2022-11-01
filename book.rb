00ce12267ff2f82ad08b472c97233938061f74e8
basitali111 <allahbadshah148@gmail.com>
2022-11-01 20:20:08 +0500

    fixing linetrs

--- book.rb
+++ book.rb
@@ -1,16 +1,16 @@
-require  './item'
+require './item'
 
 class Book < Item
-    def initialize(publisher, cover_state, publish_date)
-        super(publish_date)
-        @publisher = publisher
-        @cover_state = cover_state 
-    end
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def can_be_archived?
-        super || @cover_state == 'bad'
-    end
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
 end
 
 book = Book.new('basit', 'bad', '2022-09-08')
-puts book.can_be_archived?()
+puts book.can_be_archived?
--- label.rb
+++ label.rb
@@ -1,15 +1,16 @@
-require './item.rb'
+require './item'
 class Label
-    attr_reader :items
-    def initialize(title, color)
-        @id = Random.rand(1..1000)
-        @title = title
-        @color = color
-        @items = []
-    end
+  attr_reader :items
 
-    def add_item(item)
-        @items.push(item)
-        item.label = self
-    end
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.label = self
+  end
 end
